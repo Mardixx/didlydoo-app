b5ddbd1e460aea89ecc40ac5a5e6de3e94b22127
antoinel74 <antoine.lansman@gmail.com>
2023-10-13 16:35:10 +0200

    trying to post the add user  on server

--- JS/Components/_addUser.js
+++ JS/Components/_addUser.js
@@ -14,11 +14,30 @@ export function addAttendeeFormToCard(eventData, project) {
     checkbox.type = "checkbox";
     checkbox.id = dateValue;
     checkbox.name = "dateAvailability";
-    checkbox.checked = false;
+    checkbox.checked = false; // <- This is the default value //
 
     addAttendeeForm.appendChild(checkbox);
   });
   addAttendeeForm.appendChild(attendeeNameInput);
   addAttendeeForm.appendChild(submitAttendeeButton);
   project.appendChild(addAttendeeForm);
+
+  const userAvailability = {
+    name: attendeeNameInput.value,
+    availability: checkbox.value,
+  };
 }
+
+// POST THE INPUT TO THE JSON //
+let postUserAvailability = () => {
+  fetch("http://localhost:3000/api/events/", {
+    method: "POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+};
