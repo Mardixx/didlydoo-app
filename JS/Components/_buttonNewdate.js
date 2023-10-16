f1897f8d59a9d50bc8735cea2ba431d55949a0c2
antoinel74 <antoine.lansman@gmail.com>
2023-10-16 10:18:40 +0200

    merging complete

--- JS/Components/_buttonNewdate.js
+++ JS/Components/_buttonNewdate.js
@@ -1,28 +1,27 @@
 async function getData() {
-    fetch("http://localhost:3000/api/events/")
-        .then((response) => response.json())
-        .then((data) => {
-        console.log(data);
-        })
-        .catch((error) => console.error(error));
-    }
+  fetch("http://localhost:3000/api/events/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.error(error));
+}
 
 getData();
 
-document.addEventListener('DOMContentLoaded', () => {
-    proposerDate.addEventListener('click', () => {
-        let eventID = document.getElementById('eventID').value;
-        let dateToAdd = document.getElementById('newDate').value;
-        fetch(`http://localhost:3000/api/events/${eventID}/add_dates`, {
-            method: "POST",
-            body: JSON.stringify({ 
-                dates: [`${dateToAdd}`] 
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        }
-        );
+document.addEventListener("DOMContentLoaded", () => {
+  proposerDate.addEventListener("click", () => {
+    let eventID = document.getElementById("eventID").value;
+    let dateToAdd = document.getElementById("newDate").value;
+    fetch(`http://localhost:3000/api/events/${eventID}/add_dates`, {
+      method: "POST",
+      body: JSON.stringify({
+        dates: [`${dateToAdd}`],
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
     });
+  });
 });
 // newDateProposition();
