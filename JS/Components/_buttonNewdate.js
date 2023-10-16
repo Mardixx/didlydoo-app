eac07d7ea814b7232ec9f4589185b7dabd9c6ca1
Takowski <93830775+Takowski@users.noreply.github.com>
2023-10-16 15:45:42 +0200

    innerhtml

--- JS/Components/_buttonNewdate.js
+++ JS/Components/_buttonNewdate.js
@@ -25,3 +25,33 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 // newDateProposition();
+
+export async function AddNewDate() {
+  const formNewDateHTML = `
+    <form id="add-date-form">
+      <label for="newDate">New Date:</label>
+      <input type="date" id="newDate" name="newDate">
+      <input type="submit" value="Add Date">
+    </form>
+  `;
+  const formContainer = document.createElement("div");
+  formContainer.innerHTML = formNewDateHTML;
+  document.body.appendChild(formContainer);
+
+  const addDateForm = document.getElementById("add-date-form");
+  addDateForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const eventID = document.getElementById("eventID").value;
+    const dateToAdd = document.getElementById("newDate").value;
+    fetch(`http://localhost:3000/api/events/${eventID}/add_dates`, {
+      method: "POST",
+      body: JSON.stringify({
+        dates: [`${dateToAdd}`],
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      
+      },
+    });
+  });
+}
