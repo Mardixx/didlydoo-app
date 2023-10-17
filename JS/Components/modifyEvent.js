dbc59044f511d6195f16be3ebc8216446d89cec2
antoinel74 <antoine.lansman@gmail.com>
2023-10-17 17:05:19 +0200

    add style modal

--- JS/Components/modifyEvent.js
+++ JS/Components/modifyEvent.js
@@ -3,16 +3,17 @@ export async function modifyEvent(event) {
 
   // fetch event data
   fetch(`http://localhost:3000/api/events/${eventID}`)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       console.log(data);
       createForm(data, eventID);
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
 
 function createForm(data, eventID) {
   const form = document.createElement("form");
+  form.classList.add("modalForm");
   form.innerHTML = `
     <label for="name">Name</label>
     <input type="text" id="name" name="name" value="${data.name}">
@@ -20,8 +21,10 @@ function createForm(data, eventID) {
     <input type="text" id="author" name="author" value="${data.author}">
     <label for="description">Description</label>
     <textarea id="description" name="description">${data.description}</textarea>
+    <div class="actions">
     <button type="submit" id="submit-form">Submit</button>
     <button type="button" id="cancel-button">Cancel</button>
+    </div>
   `;
   document.body.appendChild(form);
   console.log(form);
@@ -31,12 +34,12 @@ function createForm(data, eventID) {
   const descriptionInput = form.querySelector("#description");
 
   // handle form submission
-  form.addEventListener("submit", event => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = {
       name: nameInput.value,
       author: authorInput.value,
-      description: descriptionInput.value
+      description: descriptionInput.value,
     };
     submitForm(formData, eventID, form);
   });
@@ -53,15 +56,15 @@ function submitForm(formData, eventID, form) {
     method: "PATCH",
     body: JSON.stringify(formData),
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    },
   })
-    .then(response => {
+    .then((response) => {
       if (response.ok) {
         form.remove();
       } else {
         throw new Error("Failed to update event");
       }
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
