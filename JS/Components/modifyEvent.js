b3fdd5e1c9921f3082b5c199563fe592ed863253
Takowski <93830775+Takowski@users.noreply.github.com>
2023-10-17 15:28:33 +0200

    probleme d input

--- JS/Components/modifyEvent.js
+++ JS/Components/modifyEvent.js
@@ -1,50 +1,62 @@
 export async function modifyEvent(event) {
   const eventID = event.target.getAttribute("data-event-id");
-//   const confirmation = confirm(
-//     "Warning, this event will be deleted. Are u sure ?"
-//   );
-//   if (confirmation) {
-//     fetch(`http://localhost:3000/api/events/${eventID}`, {
-//       method: "DELETE",
-//     })
-//       .then((reponse) => {
-//         if (reponse.status === 200) {
-//           console.log("Delete successfull");
-//           event.target.parentElement.remove();
-//         } else {
-//           console.error("Deletion failed");
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("An error occurred:", error);
-//       });
-//   }
-// }
 
-// PATCH	/api/events/[id]/	{ name: string (optional), author: string (optional), description: string (optional) }	
-// Patches (edit) an event with the provided infos
+  // fetch event data
+  fetch(`http://localhost:3000/api/events/${eventID}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      
+      const form = document.createElement("form");
+      form.innerHTML = `
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" value="${data.name}">
+        <label for="author">Author</label>
+        <input type="text" id="author" name="author" value="${data.author}">
+        <label for="description">Description</label>
+        <textarea id="description" name="description">${data.description}</textarea>
+        <button type="submit" id="submit-form">Submit</button>
+        <button type="button" id="cancel-button">Cancel</button>
+      `;
+      document.body.appendChild(form);
 
-// console.log(eventID);GET	/api/events/[id]		A single event
+      // get form inputs
+      const nameInput = form.querySelector("#name");
+      const authorInput = form.querySelector("#author");
+      const descriptionInput = form.querySelector("#description");
 
-// fetch event data
-fetch(`http://localhost:3000/api/events/${eventID}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    
-    const form = document.createElement("form");
-    form.innerHTML = `
-    <label for="name">Name</label>
-    <input type="text" id="name" name="name" value="${data.name}">
-    <label for="author">Author</label>
-    <input type="text" id="author" name="author" value="${data.author}">
-    <label for="description">Description</label>
-    <textarea id="description" name="description">${data.description}</textarea>
-    <button type="submit">Submit</button>
-    `;
-    document.body.appendChild(form);
-  })
-  .catch(error => console.error(error));
-    
+      // create formData object
+      const formData = {
+        name: nameInput.value,
+        author: authorInput.value,
+        description: descriptionInput.value
+      };
+
+      // handle form submission
+      form.addEventListener("submit", event => {
+        event.preventDefault();
+        fetch(`http://localhost:3000/api/events/${eventID}`, {
+          method: "PATCH",
+          body: JSON.stringify(formData),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+          .then(response => {
+            if (response.ok) {
+              form.remove();
+            } else {
+              throw new Error("Failed to update event");
+            }
+          })
+          .catch(error => console.error(error));
+      });
+
+      // handle cancel button click
+      const cancelButton = document.getElementById("cancel-button");
+      cancelButton.addEventListener("click", () => {
+        form.remove();
+      });
+    })
+    .catch(error => console.error(error));
 }
-
