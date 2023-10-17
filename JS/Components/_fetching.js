6bed6f4481052d56789b0e99d1b03f889a8079d2
antoinel74 <antoine.lansman@gmail.com>
2023-10-17 10:54:43 +0200

    debug delete button

--- JS/Components/_fetching.js
+++ JS/Components/_fetching.js
@@ -6,76 +6,72 @@ export async function getInfo() {
   let response = await data.json();
   let section = document.querySelector(".project");
   for (let child of response) {
-    let h1 = document.createElement('h1')
-    let table = document.createElement('table')
-    let div = document.createElement('div')
-    let trDates = document.createElement('tr')
-    let bigDiv = document.createElement('div')
+    let h1 = document.createElement("h1");
+    let table = document.createElement("table");
+    let div = document.createElement("div");
+    let trDates = document.createElement("tr");
+    let bigDiv = document.createElement("div");
 
-    bigDiv.classList = 'bigDiv'
+    bigDiv.classList = "bigDiv";
 
-    trDates.classList = 'dateRow'
-    
-    h1.textContent = child.name
+    trDates.classList = "dateRow";
+
+    h1.textContent = child.name;
 
     let delBtn = document.createElement("button");
     delBtn.setAttribute("data-event-id", child.id);
+    delBtn.addEventListener("click", deleteEvent);
     let addBtn = document.createElement("button");
     let modBtn = document.createElement("button");
     table.appendChild(modBtn);
     table.appendChild(addBtn);
     table.appendChild(delBtn);
 
-    let divNames = document.createElement('tr')
+    let divNames = document.createElement("tr");
 
-    divNames.classList = 'nameDiv'
+    divNames.classList = "nameDiv";
     for (let attendees of child.dates[0].attendees) {
-      let td = document.createElement('td')
+      let td = document.createElement("td");
 
-      td.classList = 'names'
+      td.classList = "names";
 
-      td.textContent = attendees.name
+      td.textContent = attendees.name;
 
-      divNames.appendChild(td)
-      bigDiv.appendChild(divNames)
+      divNames.appendChild(td);
+      bigDiv.appendChild(divNames);
       console.log(attendees);
     }
 
-    div.appendChild(h1)
-    div.appendChild(trDates)
-    div.appendChild(table)
-    bigDiv.appendChild(div)
-    section.appendChild(bigDiv)
+    div.appendChild(h1);
+    div.appendChild(trDates);
+    div.appendChild(table);
+    bigDiv.appendChild(div);
+    section.appendChild(bigDiv);
 
-    
-    
     for (let dates of child.dates) {
       let td = document.createElement("td");
 
-      td.classList = 'dates'
+      td.classList = "dates";
       td.textContent = dates.date;
 
-      trDates.appendChild(td)
+      trDates.appendChild(td);
 
-      let tr = document.createElement('tr')
+      let tr = document.createElement("tr");
 
       for (let names of dates.attendees) {
-
-        let td = document.createElement('td')
+        let td = document.createElement("td");
 
         if (names.available === null) {
-          td.textContent = "Don't know"
-        }else {
-          td.textContent = names.available
+          td.textContent = "Don't know";
+        } else {
+          td.textContent = names.available;
         }
-        tr.appendChild(td)
-        table.appendChild(tr)
+        tr.appendChild(td);
+        table.appendChild(tr);
       }
     }
-    
 
-
-/*     let delBtn = document.createElement('button')
+    /*     let delBtn = document.createElement('button')
     let addBtn = document.createElement('button')
     let modBtn = document.createElement('button')
 
@@ -83,7 +79,7 @@ export async function getInfo() {
     table.appendChild(modBtn)
     table.appendChild(addBtn)
     table.appendChild(delBtn) */
-    
+
     addAttendeeFormToCard(child, div);
   }
 }
